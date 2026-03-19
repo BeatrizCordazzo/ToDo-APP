9baa411ac9f1f8a614e73d311e760d8ac9f6911f
BeatrizCordazzo <cordazzobea@gmail.com>
2026-03-19 13:08:31 +0100

    refactor: deleted dockerfile

--- config/db.php
+++ config/db.php
@@ -1,12 +1,11 @@
 <?php
 
-$host='localhost'; // Database host
-$dbname='todo_app'; // Database name
-$username='root'; // Database username
-$password='1234'; // Database password
+$dsn = getenv('DB_DSN') ?: "mysql:host=localhost;dbname=todo_app";
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: '1234';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
